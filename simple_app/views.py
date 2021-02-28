e6a03d85b002fe4b20472f64320d85df094ff278
devcrack <jaha_devcrack@outlook.com>
2021-02-28 15:18:54 -0600

    #5 View for Fist page is ready for be extend it

--- simple_app/views.py
+++ simple_app/views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+
 class FirstPageView(TemplateView):
     template_name = "page-1.html"
+
+    def get_context_data(self, *args, **kwargs):
+        self.context = super(FirstPageView, self).get_context_data()
+        self.context['PAGE_TITLE'] = "First_Page"
+        return self.context
+
+    def get(self, request, *args, **kwargs):
+        self.get_context_data(request)
+        return self.render_to_response(self.context)

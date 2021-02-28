d8c044206744d158716009b6c764a5bb24db1ca0
devcrack <jaha_devcrack@outlook.com>
2021-02-28 15:00:52 -0600

    #5 View for Fist page is ready for be extend it

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import TemplateView
+
+class FirstPageView(TemplateView):
+    template_name = "page-1.html"

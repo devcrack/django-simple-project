4ff01fb3b52ad27e84850b99dc162c7320da65b1
devcrack <jaha_devcrack@outlook.com>
2021-02-28 14:59:44 -0600

    #4 Redirect to first page is Done!

--- simple_project/urls.py
+++ simple_project/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from distro import name
 from django.contrib import admin
 from django.urls import path
+from simple_app.views import FirstPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', FirstPageView.as_view(), name="first_page"),
 ]

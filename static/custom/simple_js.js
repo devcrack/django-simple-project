0d291233914727b3d1ac4694a59d9a843908ed60
devcrack <jaha_devcrack@outlook.com>
2021-03-01 02:39:58 -0600

    NO MORE Vuejs for this simple exerscie

--- static/custom/simple_js.js
+++ static/custom/simple_js.js
@@ -1,12 +1,32 @@
 $(function () {
     new Vue({
         el: "#simple",
-        data:{
-            names:"Ini Name"
+        data: {
+            names: "",
+            address: "",
+            phone: ""
         },
-        methods:{
+        methods: {
             save() {
-                console.log("HOLIS");
+                let form_data = new FormData();
+
+                    form_data.append("name", this.names);
+                    form_data.append("address", this.address);
+                    form_data.append("address", this.phone);
+
+                axios.post('/', form_data).then(response => {
+                        console.log(response);
+                        if (response.data.errors.length)
+                            console.log(response.data.errors);
+                        else
+                            window.location.href = '/';
+                    }
+                ).catch(
+                    error => {
+                        console.log("error");
+                        console.log(error);
+                    }
+                );
             },
         }
     });

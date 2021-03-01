daf7f9764c054188f736b3725e084aa7abe9c721
devcrack <jaha_devcrack@outlook.com>
2021-03-01 03:41:06 -0600

    #8  Completed

--- simple_app/views.py
+++ simple_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.shortcuts import redirect
+from django.urls import reverse
 
 @method_decorator(csrf_exempt, name='dispatch')
 class FirstPageView(TemplateView):
@@ -26,5 +27,18 @@ class FirstPageView(TemplateView):
         name = self.request.POST['names']
         address = self.request.POST['address']
         phone = self.request.POST['phone']
-        return redirect("first_page")
-        # return redirect()
+        return redirect(reverse('second_page', kwargs={'name': name, 'address': address, 'phone': phone}))
+
+
+class SecondPage(TemplateView):
+    template_name = "page-2.html"
+
+    def get_context_data(self, *args, **kwargs):
+        self.context = super(SecondPage, self).get_context_data()
+        self.context['PAGE_TITLE'] = "Second Page"
+        return self.context
+
+    def get(self, request, *args, **kwargs):
+        self.get_context_data(request)
+        self.context['data'] = [f"{data}:  {kwargs[data]}" for data in kwargs]
+        return self.render_to_response(self.context)

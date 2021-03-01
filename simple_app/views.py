c4c00b32ee76e4d217adbcbde83a2c688712bc38
devcrack <jaha_devcrack@outlook.com>
2021-03-01 02:40:29 -0600

    No more  CSRF Authetication required

--- simple_app/views.py
+++ simple_app/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.shortcuts import redirect
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class FirstPageView(TemplateView):
     template_name = "page-1.html"
 
@@ -16,3 +19,12 @@ class FirstPageView(TemplateView):
     def get(self, request, *args, **kwargs):
         self.get_context_data(request)
         return self.render_to_response(self.context)
+
+    def post(self, *args, **kwargs):
+        if not self.request.POST:
+            return redirect("first_page")
+        name = self.request.POST['names']
+        address = self.request.POST['address']
+        phone = self.request.POST['phone']
+        return redirect("first_page")
+        # return redirect()

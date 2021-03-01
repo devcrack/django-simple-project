8b6283073b222923d17a31c11151b883cb63a872
devcrack <jaha_devcrack@outlook.com>
2021-03-01 03:42:30 -0600

    No info is needed

--- simple_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class SimpleInfo(models.Model):
-    name = models.CharField(null=False, blank=False, max_length=256)
-    address = models.CharField(null=False, blank=False, max_length=256)
-    number_phone = models.CharField(null=False, blank=False, max_length=256)
